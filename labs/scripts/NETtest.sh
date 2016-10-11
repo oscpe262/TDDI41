01a7b6b4f88565eb5e917c3a8b1a9e75007f17b7
oscpe262 <oscpe262@student.liu.se>
2016-10-11 11:54:45 +0200

    NET for turn-in 1

--- labs/scripts/NETtest.sh
+++ labs/scripts/NETtest.sh
@@ -2,53 +2,57 @@
 gr="\e[1;32m"
 rd="\e[1;31m"
 def="\e[0m"
+greentext="$gr%s$def\n"
+redtext="$rd%s$def\n"
 
 # Client-1 test
 if [ `uname -n` == "client-1" ]
 then
-  printf "$gr%s$def\n" "hostname ok"
+  printf "${greentext}" "hostname ok"
 else
-  printf "$rd%s$def\n" "hostname nok"
+  printf "${redtext}" "hostname nok"
   #exit 1
 fi
 
-ping -c 1 130.236.178.153
+ping -c 1 130.236.178.153 &> /dev/null
 if [ $? -eq 0 ]; then
-  :
+  printf "${greentext}" "Ping 130.236.178.153 successful!"
 else
-  printf "$rd%s$def\n" "Could not connect to gw (local, IP)."
+  printf "${redtext}" "Could not connect to gw (local, IP)."
   exit 1
 fi
 
-ping -c 1 130.236.178.17
+ping -c 1 130.236.178.17 &> /dev/null
 if [ $? -eq 0 ]; then
-  :
+  printf "${greentext}" "Ping 130.236.178.17 successful!"
 else
-  printf "$rd%s$def\n" "Could not connect to gw (ext, IP)."
+  printf "${redtext}" "Could not connect to gw (ext, IP)."
   exit 1
 fi
 
-ping -c 1 130.236.178.1
+ping -c 1 130.236.178.1 &> /dev/null
 if [ $? -eq 0 ]; then
-  :
+  printf "${greentext}" "Ping 130.236.178.1 successful!"
 else
-  printf "$rd%s$def\n" "Could not connect to ida-gw (IP)."
+  printf "${redtext}" "Could not connect to ida-gw (IP)."
   exit 1
 fi
 
 
-ping -c 1 ida-gw.sysinst.ida.liu.se
+ping -c 1 ida-gw.sysinst.ida.liu.se &> /dev/null
 if [ $? -eq 0 ]; then
-  :
+  printf "${greentext}" "Ping ida-gw.sysinst.ida.liu.se successful!"
 else
-  printf "$rd%s$def\n" "Could not connect to ida-gw (name resolved)."
+  printf "${redtext}" "Could not connect to ida-gw (name resolved)."
   exit 1
 fi
 
-ping -c 1 www.google.com
+ping -c 1 www.google.com &> /dev/null
 if [ $? -eq 0 ]; then
-  printf "$gr%s$def\n" "We have world-wide connectivity"
+  printf "${greentext}" "Ping www.google.com successful!"
+  printf "${greentext}" "We have world-wide connectivity"
 else
-  printf "$rd%s$def\n" "No world-wide connectivity"
+  printf "${redtext}" "No world-wide connectivity"
   exit 1
 fi
+exit 0
